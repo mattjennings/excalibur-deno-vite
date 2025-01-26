dacb8b2dd67bb57a6f419d6da457dae959bb763b
Matt Jennings <me@mattjennings.io>
2025-01-26 16:30:50 -0600

    a

--- webview.ts
+++ webview.ts
@@ -1,15 +1,16 @@
 /// <reference lib="deno.ns" />
 import { createWebView } from "jsr:@justbe/webview";
+import { Hono } from "jsr:@hono/hono@4.6.19"
+import { serveStatic } from 'jsr:@hono/hono@4.6.19/deno'
 const DEVELOPMENT = Deno.env.get("DENO_ENV") === "development";
 
 // serve dist folder
 if (!DEVELOPMENT) {
-  new Worker(
-    new URL("./webview-server.ts", import.meta.url).href,
-    {
-      type: "module",
-    },
-  );
+  const app = new Hono()
+
+  app.use('*', serveStatic({ root: import.meta.dirname + '/dist/web' }))
+  app.get('*', serveStatic({ path: import.meta.dirname + '/dist/web/index.html' }))
+  Deno.serve(app.fetch)
 }
 
 using webview = await createWebView({
