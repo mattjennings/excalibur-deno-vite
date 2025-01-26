f618c293bcd861a04a7c4cacef5bf75c4a36ef48
Matt Jennings <me@mattjennings.io>
2025-01-26 17:08:58 -0600

    a

--- webview.ts
+++ webview.ts
@@ -1,19 +1,17 @@
 /// <reference lib="deno.ns" />
 import { createWebView } from "jsr:@justbe/webview";
-import { Hono } from "jsr:@hono/hono@4.6.19"
-import { serveStatic } from 'jsr:@hono/hono@4.6.19/deno'
 import * as posix from "jsr:@std/path/posix";
-
+import { serveDir } from "jsr:@std/http/file-server";
 
 const DEVELOPMENT = Deno.env.get("DENO_ENV") === "development";
 
 // serve dist folder
 if (!DEVELOPMENT) {
-  const app = new Hono()
-
-  app.use('*', serveStatic({ root: posix.join(import.meta.dirname!, '/dist/web') }))
-  app.get('*', serveStatic({ path: posix.join(import.meta.dirname!, '/dist/web/index.html') }))
-  Deno.serve(app.fetch)
+  Deno.serve((req) => {
+    return serveDir(req, {
+      fsRoot: posix.join(import.meta.dirname!, "/dist/web"),
+    });
+  });
 }
 
 using webview = await createWebView({
@@ -23,8 +21,7 @@ using webview = await createWebView({
   size: {
     width: 1280,
     height: 720,
-  },  
+  },
 });
-
 
 await webview.waitUntilClosed();
